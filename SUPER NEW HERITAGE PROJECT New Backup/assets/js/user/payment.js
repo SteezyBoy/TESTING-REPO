998bf3caf21b6502ec95fd968f42836e98ce2286
Frank Wuaten <93098942+SteezyBoy@users.noreply.github.com>
2026-06-15 08:49:45 +0800

    Delete SUPER NEW HERITAGE PROJECT New Backup directory

--- SUPER NEW HERITAGE PROJECT New Backup/assets/js/user/payment.js
+++
@@ -1,5 +0,0 @@
-function closeCashierWaitScreen() {
-    stopCashierPaymentPoll();
-    document.getElementById("cashier-wait-screen").style.display = "none";
-    document.getElementById("cartModal").style.display = "none";
-}
